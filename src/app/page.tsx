4a40314118dacf74b4d0b70351e134131d5a3364
seungmin <bang3957@gmail.com>
2025-09-03 23:32:04 +0900

    fix: 1440

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,9 @@ import Link from "next/link"
 
 export default function Home() {
   return (
-    <div className="flex flex-col ">
+    <div className="flex flex-col w-full">
       <Header />
-      <main className="flex flex-col items-center w-full px-[50px]">
+      <main className="flex flex-col items-center w-full">
         <section className="flex">
           <Image src="/images/img_main_header.png" alt="line" width={1905} height={1270} />
         </section>
